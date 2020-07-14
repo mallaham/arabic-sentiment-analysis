9b98d7e271f212f1575594ded4614d4dc567435a
Mowafak Allaham <mmallaham1@gmail.com>
2020-07-14 13:55:13 -0500

    refactor: add new functionality to load pickle

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logging import info, basicConfig, INFO
 import logging
 import os
 import pickle
+from datetime import datetime as dt
 
 import pandas as pd
 from gensim.models import KeyedVectors
@@ -122,6 +123,15 @@ class SentimentAnalyzer(DataWorker):
 
         return [classifier_accuracy, classifier_f1_score, classifier_precision, classifier_recall]
 
+    @staticmethod
+    def load_pickle(path):
+        try:
+            with open(path, "rb") as pickle_file:
+                return pickle.load(pickle_file)
+        except FileNotFoundError:
+            logging.info("Pickle file not found. Exiting...")
+            exit(1)
+
     def feature(self, words):
         """average words' vectors"""
 
@@ -171,26 +181,27 @@ class SentimentAnalyzer(DataWorker):
         text_vectors = self.remove_nan(text_vectors)
         return text_vectors
 
-    def predict(self, x_test):
+    def scoring(self, x_test, y_test=None, path_to_model=None):
+        # load pickled models
+        if path_to_model:
+            logging.info("Loading Model")
+            self.fit_models = self.load_pickle(path_to_model)
+        # make predictions
         logging.info("Making predictions...")
         for model_name, fitted_model in self.fit_models.items():
-            # predictions
             pred = fitted_model.predict(x_test)
             self.predictions.update({model_name: pred})
+            logging.info("Generated predictions for {}".format(model_name))
+            if y_test:
+                scoring_metrics = self.calculate_metrics(pred, y_test)
+                string = '{} | Accuracy. {:.2f}% F1. {:.2f}% Precision. {:.2f} Recall. {:.2f}'
+                logging.info(string.format(model_name, scoring_metrics[0] * 100, scoring_metrics[1] * 100,
+                                scoring_metrics[2] * 100, scoring_metrics[3] * 100))
             negative = len(pred[pred == 0])
             positive = len(pred[pred == 1])
-            string = '\t{} | Positive Tweets. {:.2f}% Negative Tweets. {:.2f}% '
-            print(string.format(model_name, positive*100/pred.shape[0], negative*100/pred.shape[0]))
+            string = '{} | Positive Tweets. {:.2f}% Negative Tweets. {:.2f}% '
+            logging.info(string.format(model_name, positive*100/pred.shape[0], negative*100/pred.shape[0]))
         return self.predictions
-
-    def scoring(self, x_test, y_test):
-        logging.info("Scoring classifiers...")
-        for model_name, fitted_model in self.fit_models.items():
-            pred = fitted_model.predict(x_test)
-            scoring_metrics = self.calculate_metrics(pred, y_test)
-            string = '\t{} | Accuracy. {:.2f}% F1. {:.2f}% Precision. {:.2f} Recall. {:.2f}'
-            print(string.format(model_name, scoring_metrics[0] * 100, scoring_metrics[1] * 100,
-                                scoring_metrics[2] * 100, scoring_metrics[3] * 100))
 
     def train_model(self, x_train, y_train, save_model=False, cross_val=False, kfolds=2):
 
@@ -213,6 +224,7 @@ class SentimentAnalyzer(DataWorker):
                 self.cross_validate(c, x_train, y_train, kfolds)  # TODO: return predictions, plot ROC
             else:
                 model = c.fit(x_train, y_train)
+                logging.info("Finished training {}".format(classifier_name))
                 self.fit_models.update({classifier_name: model})
                 if save_model:
                     with open("./{}.sav".format(classifier_name), "wb") as file:
@@ -237,31 +249,51 @@ if __name__ == "__main__":
     parser.add_argument("--vectors", help="path a pre-trained vectors model.")
     parser.add_argument("--train_dataset", help="path a labeled (0/1) sentiment dataset.", required=False)
     parser.add_argument("--score_dataset", help="path to data for sentiment analysis.", required=True)
+    parser.add_argument("--load_models", help="path to load models", required=False)
     # parser.add_argument("--save_models", help="path to save models", required=False, default=False)
-    # parser.add_argument("--load_models", help="path to load models", required=False)
     # parser.add_argument("--cross_validation", help="flag to run cross validation", required=False, default=False)
 
+    # pars args
     args = parser.parse_args()
     vec = args.vectors
     dataset_path = args.train_dataset #"datasets/mpqa-ar.csv"
     score_data = args.score_dataset
+    load_model = args.load_models
     # save_models = args.save_models
+    tag = dt.now().strftime("%Y_%m_%d_%H_%H_%M_%S")
     train_data = None
     test_data = None
     train_target = None
     test_target = None
-
+    predictions = None
+    user_id = None
+    path_to_pickle = None
     arSA = SentimentAnalyzer(vec, dataset_path, score_data)
 
     if dataset_path:
+        user_id = arSA.test_data['user_id']
         train_data = arSA.process_data(arSA.train_data['txt'])
         train_target = arSA.train_data['sentiment']
 
     if score_data:
-        test_data = arSA.process_data(arSA.test_data['txt'])
-        test_target = arSA.test_data['sentiment']
+        test_data = arSA.process_data(arSA.test_data['tweet'])
+        try:
+            test_target = arSA.test_data['sentiment']
+            # train model
+        except KeyError:
+            logging.info("Target variable is not in the test data...")
+            pass
 
-    arSA.train_model(train_data, train_target)
-    arSA.scoring(test_data, test_target)
+        if not load_model:
+            arSA.train_model(train_data, train_target)
+            predictions = arSA.scoring(test_data, test_target)
+        else:
+            # scoring
+            predictions = arSA.scoring(test_data, path_to_pickle)
+
+    for model, pred in predictions.items():
+        pred = pd.DataFrame(pred, columns=['sentiment'])
+        output = pd.DataFrame({"ID": user_id, "tweet": arSA.test_data['tweet'], "sentiment": pred['sentiment']})
+        output.to_csv("./output/" + model + "_" + tag + "_predictions.csv")
 
     logging.info("Process is complete...")
